8130fd6b1b1ee8e9625555256f0ab3315e56d3a2
Growtopia Jaw <growtopiajaw@gmail.com>
2018-12-02 21:23:00 +0800

    Vendorize everything Fix LED light Try to be less dependent on other device's blobs Initial testing stage

--- device.mk
+++ device.mk
@@ -268,8 +268,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.aw2013
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
